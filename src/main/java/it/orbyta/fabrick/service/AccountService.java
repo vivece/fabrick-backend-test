a5d494a1f3fdac2266da9d130a31239dc1f1daea
vivece <vece.vittorio@gmail.com>
2026-05-16 10:52:21 +0200

    fix pipeline

--- src/main/java/it/orbyta/fabrick/service/AccountService.java
+++ src/main/java/it/orbyta/fabrick/service/AccountService.java
@@ -2,7 +2,6 @@ package it.orbyta.fabrick.service;
 
 import it.orbyta.fabrick.client.FabrickClient;
 import it.orbyta.fabrick.dto.response.BalanceResponse;
-import it.orbyta.fabrick.repository.TransactionRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
