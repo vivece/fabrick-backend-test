f13c5232336e29d3fb25553c71e86d61ee6fd9b8
vivece <vece.vittorio@gmail.com>
2026-05-16 16:51:21 +0200

    AccountRequest has been restored

--- src/main/java/it/orbyta/fabrick/dto/request/moneyTransfer/Account.java
+++ src/main/java/it/orbyta/fabrick/dto/request/moneyTransfer/Account.java
@@ -8,6 +8,11 @@ import javax.validation.constraints.NotBlank;
 @Getter
 @Setter
 public class Account {
+
+    @NotBlank(message = "creditor.account.accountCode is required")
     private String accountCode;
+
+    //TODO implementa un customValidator (Constraint Validator?) per la specifica "The BIC code of the creditor bank. Required if a SWIFT account number is provided as accountCode. Optional if an IBAN code is provided as accountCode."
     private String bicCode;
+
 }
