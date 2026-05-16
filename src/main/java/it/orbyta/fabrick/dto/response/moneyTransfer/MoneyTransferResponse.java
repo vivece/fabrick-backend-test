0759405d6c94fd67b1ab7dc1db33392aad59636a
vivece <vece.vittorio@gmail.com>
2026-05-16 16:32:19 +0200

    fixPipeline

---
+++ src/main/java/it/orbyta/fabrick/dto/response/moneyTransfer/MoneyTransferResponse.java
@@ -0,0 +1,36 @@
+package it.orbyta.fabrick.dto.response.moneyTransfer;
+
+import it.orbyta.fabrick.dto.request.moneyTransfer.Creditor;
+import it.orbyta.fabrick.dto.response.Amount;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+public class MoneyTransferResponse {
+
+    private String moneyTransferId;
+    private String status;
+    private String direction;
+    private Creditor creditor;
+    private Debtor debtor;
+    private String cro;
+    private String trn;
+    private String uri;
+    private String description;
+    private LocalDate createdTime;
+    private LocalDate accountedTime;
+    private LocalDate debtorValueDate;
+    private LocalDate creditorValueDate;
+    private Amount amount;
+    private Boolean isUrgent;
+    private Boolean isInstant;
+    private String feeType;
+    private String feeAccountId;
+    private Boolean hasTaxRelief;
+    private List<Fee> fees;
+    
+}
